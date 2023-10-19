e14bcf0235964b7116ad873ed0ea067879f8ed7c
vetri1203 <vetriveljaganathan5125@gmail.com>
2023-10-20 01:55:46 +0530

    add permission

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors({origin:`https://user-api-syot.onrender.com/api`}));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Add other necessary CORS headers here
     next();
   });
